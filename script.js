23d2a866f8d60ad5924724266861c115e3af4a27
Arjun1Alpha <146172580+Arjun1Alpha@users.noreply.github.com>
2025-08-11 18:42:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,175 @@
+const lenis = new Lenis();
+
+lenis.on("scroll", ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", function () {
+  gsap.to("#home .backLayer", {
+    y: "50vh",
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#home",
+      start: "top 0%",
+      end: "bottom 0%",
+      scrub: true,
+      onLeave: () => {
+        if (window.innerWidth > 768) {
+          document.getElementById("form").style.transform = "translateY(0%)";
+        } else {
+          document.querySelector(".fixedBtn").style.transform =
+            "translateY(0%)";
+        }
+      },
+      onEnterBack: () => {
+        if (window.innerWidth > 768) {
+          document.getElementById("form").style.transform = "translateY(100%)";
+        } else {
+          document.querySelector(".fixedBtn").style.transform =
+            "translateY(100%)";
+        }
+      },
+    },
+  });
+
+  gsap.to(
+    "#home .content .logo, #home .content h1, #home .content .split .left > p, #home .content .split .left .tiles",
+    {
+      y: 0,
+      opacity: 1,
+      ease: "power1.out",
+      stagger: 0.3,
+    }
+  );
+
+  gsap.to("#home .content .split .form", {
+    y: 0,
+    opacity: 1,
+    ease: "power1.out",
+    duration: 1,
+  });
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#second",
+      start: "top center",
+    },
+  });
+
+  tl.from("#second .imgg5", { y: "15%", ease: "power4.out", duration: 1 }, "0") // fifth
+    .from("#second .imgg4", { y: "25%", ease: "power4.out", duration: 1 }, "0") // forth
+    .from("#second .imgg2", { y: "35%", ease: "power4.out", duration: 1 }, "0") // third
+    .from("#second .imgg3", { y: "50%", ease: "power4.out", duration: 1 }, "0") // second
+    .from("#second .imgg1", { y: "70%", ease: "power4.out", duration: 1 }, "0") //top most
+    .from("#second .projected", {
+      y: "100px",
+      ease: "power4.out",
+      duration: 1,
+      opacity: 0,
+    })
+    .from(
+      "#second .arrow",
+      { y: "100px", x: "-100px", ease: "power4.out", duration: 1, opacity: 0 },
+      "<0.1"
+    );
+
+  gsap.from("#second .left *", {
+    y: "100px",
+    ease: "power4.out",
+    duration: 1,
+    opacity: 0,
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: "#second .left",
+      start: "top 80%",
+    },
+  });
+
+  gsap.from("#third .content *", {
+    y: "100px",
+    ease: "power4.out",
+    duration: 1,
+    opacity: 0,
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: "#third ",
+      start: "top 80%",
+    },
+  });
+
+  gsap.from("#forth .content *", {
+    y: "100px",
+    ease: "power4.out",
+    duration: 1,
+    opacity: 0,
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: "#forth ",
+      start: "top 80%",
+    },
+  });
+
+  gsap.from("#fifth  *", {
+    y: "50",
+    ease: "power4.out",
+    duration: 1,
+    opacity: 0,
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: "#fifth ",
+      start: "top 80%",
+    },
+  });
+
+  let pairs = [
+    [".pop1", ".pop3"],
+    [".pop2", ".pop4"],
+  ];
+
+  let tl1 = gsap.timeline({ repeat: -1, repeatDelay: 1 });
+
+  pairs.forEach(([el1, el2]) => {
+    tl1
+      .fromTo(
+        [el1, el2],
+        { opacity: 0 },
+        { opacity: 1, duration: 0.8, ease: "power1.inOut" }
+      )
+      .to([el1, el2], { opacity: 0, duration: 0.8, ease: "power1.inOut" });
+  });
+
+  gsap.fromTo(
+    "#third .biscuit",
+    {
+      y: -150,
+    },
+    {
+      y: 50,
+      ease: "none",
+      scrollTrigger: {
+        trigger: "#third .biscuit",
+        start: "top 100%",
+        end: "bottom 0%",
+        toggleActions: "play none none reverse",
+        scrub: true,
+      },
+    }
+  );
+
+
+
+  document.querySelector('.fixedBtn').addEventListener('click', function(){
+    document.querySelector("#form").classList.add('active')
+  })
+  document.querySelector('.close').addEventListener('click', function(){
+    document.querySelector("#form").classList.remove('active')
+  })
+
+
+});
